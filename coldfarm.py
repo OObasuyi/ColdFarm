bfa80ed75b93ac745aa0517465985782dfccd6e3
Osamuede Obasuyi <80005871+OObasuyi@users.noreply.github.com>
2024-04-10 14:44:16 +0200

    update L73

--- coldfarm.py
+++ coldfarm.py
@@ -70,6 +70,7 @@ class Farmer(Clarity):
         # combine and normalize
         aci_dfs = pd.concat(aci_dfs, axis=0).drop_duplicates(subset=['iface_mac'])
         aci_dfs['dns_name'] = aci_dfs['ip'].apply(lambda x: self._dns_socket_handle(x))
+        aci_dfs['iface_mac'] = aci_dfs['iface_mac'].apply(lambda x: x.lower())
         return aci_dfs
 
     def pull_csw_data(self):
